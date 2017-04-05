d795f731668a7ffc350b3d24e5b686e1cdf3a4a0
fengsong97 <fengsong97@gmail.com>
2017-04-05 13:49:58 +0800

    提交修改

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,36 +21,9 @@ angular.module('starter.controllers', [])
   //   {text: 'News', url: '#/app/news'}
   // ]; 
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/social/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+  if($rootScope.loginData){
+    $scope.loginData=$rootScope.loginData
+  }
 
   $scope.goto=function (jjj) {
     $state.go(jjj)
@@ -117,31 +90,19 @@ if($rootScope.datakk){
   
 })
 
-.controller('LoginCtrl', function($scope, $ionicModal, $timeout,$state) {
+.controller('LoginCtrl', function($scope,$rootScope, $ionicModal, $timeout,$state) {
 
   // Form data for the login modal
-  $scope.loginData = {};
+  $scope.loginData = {
+      username:"刘海燕",
+      password:"123456"
+  };
 
-  // Create the login modal that we will use later
-  // $ionicModal.fromTemplateUrl('templates/social/login.html', {
-  //   scope: $scope
-  // }).then(function(modal) {
-  //   $scope.modal = modal;
-  // });
-
-  // Triggered in the login modal to close it
-  // $scope.closeLogin = function() {
-  //   $scope.modal.hide();
-  // };
-
-  // Open the login modal
-  // $scope.login = function() {
-  //   $scope.modal.show();
-  // };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+      $rootScope.loginData=  $scope.loginData;
+    console.log('Doing login', $rootScope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
